c330ad9455ad50ab4a7180c56b5522241e0977b8
markus <e11809597@student.tuwien.ac.at>
2019-10-30 00:13:32 +0100

    added transmission timer

--- src/SequenceManager.cpp
+++ src/SequenceManager.cpp
@@ -218,6 +218,7 @@ void SequenceManager::StopGetSensors()
 
 void SequenceManager::GetSensors(int64 microTime)
 {
+    auto startTime = Clock::now();
     map<string, int32> sensors = LLInterface::GetAllSensors();
 
     vector<int32> vals;
@@ -251,6 +252,8 @@ void SequenceManager::GetSensors(int64 microTime)
 //        callbackThread.detach();
 	    LLInterface::TransmitSensors(microTime, sensors);
     }
+    auto currTime = Clock::now();
+    cout << "Transmission Timer elapsed: " << std::chrono::duration_cast<std::chrono::microseconds>(currTime-startTime).count() << endl;
 
 }
 
