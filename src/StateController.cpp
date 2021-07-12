03255dcf78ed825d7f7d71442b96f05f13dc1cfe
markuspinter <markus.m.pinter@gmail.com>
2021-07-12 20:20:17 +0200

    Teststand version

--- src/StateController.cpp
+++ src/StateController.cpp
@@ -20,7 +20,7 @@ void StateController::Init(std::function<void(std::string, double)> onStateChang
         this->onStateChangeCallback = std::move(onStateChangeCallback);
         // Change timestamp precision to provided precision (DB)
         logger = new InfluxDbLogger();
-        logger->Init("127.0.0.1", 8086, "testDb", "states", SECONDS);
+        logger->Init("127.0.0.1", 8086, "testDb", "states", MICROSECONDS);
         initialized = true;
     }
 }
