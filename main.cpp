857cc21b6e5e4f25f3b693bda25666ed8706a0ab
Lukas Karafiat <lukas.karafiat@spaceteam.at>
2022-08-13 18:14:46 +0200

    change: refactored config path reading into option handling
    Moved thread scheduling into own function
    
    Reviewer: Christofer Held

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-
 #include <string>
-#include <iomanip>      // std::setprecision
+#include <iomanip>	  // std::setprecision
 #include <sched.h>
 #include <csignal>
 #include <fstream>
@@ -45,108 +44,201 @@ std::thread *testThread = nullptr;
 // IMPROVE: use descriptive test function names.
 // SMELL: to complex test case split into multiple testcases
 void testFnc() {
-    try {
-        //wait a sec before executing function
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(2000ms);
+	try {
+	//wait a sec before executing function
+	using namespace std::chrono_literals;
+	std::this_thread::sleep_for(2000ms);
 
-        CANManager *manager = CANManager::Instance();
+	CANManager *manager = CANManager::Instance();
 
 //#ifdef TEST_NODE_INIT
 
-        Can_MessageData_t msg = {0};
-        msg.bit.info.buffer = DIRECT_BUFFER;
-        msg.bit.info.channel_id = GENERIC_CHANNEL_ID;
-        msg.bit.cmd_id = GENERIC_RES_NODE_INFO;
-        NodeInfoMsg_t *info = (NodeInfoMsg_t *) msg.bit.data.uint8;
-        info->firmware_version = 0xafb32dac;
-        info->channel_mask = 0x0000001F;
-        info->channel_type[0] = CHANNEL_TYPE_ADC24;
-        // info->channel_type[1] = CHANNEL_TYPE_ADC16;
-        info->channel_type[1] = CHANNEL_TYPE_DIGITAL_OUT;
-        info->channel_type[2] = CHANNEL_TYPE_DIGITAL_OUT;
-        info->channel_type[3] = CHANNEL_TYPE_DIGITAL_OUT;
-        info->channel_type[4] = CHANNEL_TYPE_PNEUMATIC_VALVE;
-        Can_MessageId_t canID = {0};
-        canID.info.direction = 0;
-        canID.info.priority = STANDARD_PRIORITY;
-        canID.info.special_cmd = STANDARD_SPECIAL_CMD;
-        canID.info.node_id = CAN_TEST_NODE_ID;
+	Can_MessageData_t msg = {0};
+	msg.bit.info.buffer = DIRECT_BUFFER;
+	msg.bit.info.channel_id = GENERIC_CHANNEL_ID;
+	msg.bit.cmd_id = GENERIC_RES_NODE_INFO;
+	NodeInfoMsg_t *info = (NodeInfoMsg_t *) msg.bit.data.uint8;
+	info->firmware_version = 0xafb32dac;
+	info->channel_mask = 0x0000001F;
+	info->channel_type[0] = CHANNEL_TYPE_ADC24;
+	// info->channel_type[1] = CHANNEL_TYPE_ADC16;
+	info->channel_type[1] = CHANNEL_TYPE_DIGITAL_OUT;
+	info->channel_type[2] = CHANNEL_TYPE_DIGITAL_OUT;
+	info->channel_type[3] = CHANNEL_TYPE_DIGITAL_OUT;
+	info->channel_type[4] = CHANNEL_TYPE_PNEUMATIC_VALVE;
+	Can_MessageId_t canID = {0};
+	canID.info.direction = 0;
+	canID.info.priority = STANDARD_PRIORITY;
+	canID.info.special_cmd = STANDARD_SPECIAL_CMD;
+	canID.info.node_id = CAN_TEST_NODE_ID;
 
-        manager->OnCANInit(0, canID.uint32, msg.uint8, sizeof(msg), utils::getCurrentTimestamp());
+	manager->OnCANInit(0, canID.uint32, msg.uint8, sizeof(msg), utils::getCurrentTimestamp());
 
-        std::this_thread::sleep_for(1000ms);
+	std::this_thread::sleep_for(1000ms);
 
 //#endif
 
 //#ifdef TEST_DATA
 
-        msg = {0};
-        msg.bit.info.buffer = DIRECT_BUFFER;
-        msg.bit.info.channel_id = GENERIC_CHANNEL_ID;
-        msg.bit.cmd_id = GENERIC_RES_DATA;
-        SensorMsg_t sensorMsg = {0};
-        sensorMsg.channel_mask = 0x0000001D;
+	msg = {0};
+	msg.bit.info.buffer = DIRECT_BUFFER;
+	msg.bit.info.channel_id = GENERIC_CHANNEL_ID;
+	msg.bit.cmd_id = GENERIC_RES_DATA;
+	SensorMsg_t sensorMsg = {0};
+	sensorMsg.channel_mask = 0x0000001D;
 
-        Can_MessageId_t dataCanID = {0};
-        dataCanID.info.direction = 1;
-        dataCanID.info.priority = STANDARD_PRIORITY;
-        dataCanID.info.special_cmd = STANDARD_SPECIAL_CMD;
-        dataCanID.info.node_id = CAN_TEST_NODE_ID;
+	Can_MessageId_t dataCanID = {0};
+	dataCanID.info.direction = 1;
+	dataCanID.info.priority = STANDARD_PRIORITY;
+	dataCanID.info.special_cmd = STANDARD_SPECIAL_CMD;
+	dataCanID.info.node_id = CAN_TEST_NODE_ID;
 
-        uint8_t counter = 0;
-        while (running) {
-            //adc24
-            sensorMsg.channel_data[0] = 0x20;
-            sensorMsg.channel_data[1] = 0x00;
-            sensorMsg.channel_data[2] = counter;
-            //digital out
-            sensorMsg.channel_data[3] = counter;
-            sensorMsg.channel_data[4] = 0x04;
-            sensorMsg.channel_data[5] = counter;
-            sensorMsg.channel_data[6] = 0x00;
-            sensorMsg.channel_data[7] = 0x00;
-            sensorMsg.channel_data[8] = 0x00;
-            sensorMsg.channel_data[9] = counter;
-            sensorMsg.channel_data[10] = 0xA0;
-            std::copy_n((uint8_t *) &sensorMsg.channel_mask, 4, msg.bit.data.uint8);
-            std::copy_n(sensorMsg.channel_data, 11, &msg.bit.data.uint8[4]);
+	uint8_t counter = 0;
+	while (running) {
+		//adc24
+		sensorMsg.channel_data[0] = 0x20;
+		sensorMsg.channel_data[1] = 0x00;
+		sensorMsg.channel_data[2] = counter;
+		//digital out
+		sensorMsg.channel_data[3] = counter;
+		sensorMsg.channel_data[4] = 0x04;
+		sensorMsg.channel_data[5] = counter;
+		sensorMsg.channel_data[6] = 0x00;
+		sensorMsg.channel_data[7] = 0x00;
+		sensorMsg.channel_data[8] = 0x00;
+		sensorMsg.channel_data[9] = counter;
+		sensorMsg.channel_data[10] = 0xA0;
+		std::copy_n((uint8_t *) &sensorMsg.channel_mask, 4, msg.bit.data.uint8);
+		std::copy_n(sensorMsg.channel_data, 11, &msg.bit.data.uint8[4]);
 
 
-            manager->OnCANRecv(0, dataCanID.uint32, msg.uint8, sizeof(msg), utils::getCurrentTimestamp());
-            std::this_thread::sleep_for(100ms);
-            counter++;
-        }
+		manager->OnCANRecv(0, dataCanID.uint32, msg.uint8, sizeof(msg), utils::getCurrentTimestamp());
+		std::this_thread::sleep_for(100ms);
+		counter++;
+	}
 //#endif
 
 //#ifdef TEST_SPEAKER
-        while (!controller->IsInitialized()) {
-            std::this_thread::sleep_for(1000ms);
-        }
-        Debug::error("setting test node state...");
-        StateController *stateController = StateController::Instance();
-        std::vector<std::string> states = {"testNode", "testNodeGetDiv", "testNodeSetDiv"};
-        stateController->AddUninitializedStates(states);
-        stateController->SetState("testNode", 1, std::chrono::high_resolution_clock::now().time_since_epoch().count());
-        stateController->SetState("testNodeGetDiv", 1,
-                                  std::chrono::high_resolution_clock::now().time_since_epoch().count());
-        stateController->SetState("testNodeSetDiv", 1,
-                                  std::chrono::high_resolution_clock::now().time_since_epoch().count());
+	while (!controller->IsInitialized()) {
+		std::this_thread::sleep_for(1000ms);
+	}
+	Debug::error("setting test node state...");
+	StateController *stateController = StateController::Instance();
+	std::vector<std::string> states = {"testNode", "testNodeGetDiv", "testNodeSetDiv"};
+	stateController->AddUninitializedStates(states);
+	stateController->SetState("testNode", 1, std::chrono::high_resolution_clock::now().time_since_epoch().count());
+	stateController->SetState("testNodeGetDiv", 1,
+				  std::chrono::high_resolution_clock::now().time_since_epoch().count());
+	stateController->SetState("testNodeSetDiv", 1,
+				  std::chrono::high_resolution_clock::now().time_since_epoch().count());
 //#endif
-    }
-    catch (std::exception &e) {
-        Debug::print("error: %s", e.what());
-        testThread = nullptr;
-        raise(SIGTERM);
-    }
+	}
+	catch (std::exception &e) {
+	Debug::print("error: %s", e.what());
+	testThread = nullptr;
+	raise(SIGTERM);
+	}
 }
 
 #endif
 
-// SMELL: this is a not needed static global variable (make it local)
-static int latency_target_fd = -1;
-static int32_t latency_target_value = 0;
+void signalHandler(int signum) {
+	running = 0;
+	// SMELL: absolute useless statement.
+	signum = signum;
+	// BUG: not signal safe. (join, Debug::error,close,exit ,...) not signal safe. This function is a nightmare for safety!!!
+	// IMPROVE: exit outside of main. (exit should only be used in main)
+#ifdef TEST_LLSERVER
+	if (testThread != nullptr && testThread->joinable())
+	{
+	testThread->join();
+	delete testThread;
+	}
 
+#endif
+
+	Debug::error("posix signal fired: %d, shutting down...", signum);
+
+	try {
+	LLController::Destroy();
+	}
+	catch (std::exception &e) {
+	Debug::error("signal handler: failed to shutdown LLController, %s", e.what());
+	}
+
+	Debug::close();
+
+	exit(signum);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct options {
+	char *config_path = "config";
+};
+
+void
+usage(
+	void
+) {
+	extern char *__progname;
+
+	fprintf(stderr, "usage: %s [-c configfile]\n", __progname);
+
+	exit(EXIT_FAILURE);
+}
+
+void
+get_options(
+	int argc,
+	char **argv,
+	struct options *options
+) {
+	extern char *optarg;
+	extern int errno;
+
+	int option;
+	while ((option = getopt(argc, argv, "c:")) != -1) {
+		switch (option) {
+		case 'c':
+			options->option_path = optarg;
+			break;
+
+		default:
+			usage();
+		};
+	}
+
+	return;
+}
+
+// IMPROVE: This makes the Code only compilable on linux. Make it optional for test version. (enables Multiplatform development)
+void
+set_scheduling_priority(
+	int priority
+) {
+	struct sched_param scheduling_parameters{};
+	scheduling_parameters.sched_priority = 60;
+
+	sched_setscheduler(0, SCHED_FIFO, &sp);
+
+	return;
+}
+
+// TODO: check documentation of power management behaviour
 /* Latency trick
  * if the file /dev/cpu_dma_latency exists,
  * open it and write a zero into it. This will tell 
@@ -157,114 +249,76 @@ static int32_t latency_target_value = 0;
  * 
  * Documentation/power/pm_qos_interface.txt
  */
-static void set_latency_target(void) {
-    // IMPROVE: uninitialized struct type
-    struct stat s;
-    int ret;
-    // IMPROVE: this is not multi-platform but necessary for this program
-    // SMELL: this is C code embedded in C++
-    if (stat("/dev/cpu_dma_latency", &s) == 0) {
-        // SMELL: open is only used in old C and not C++
-        latency_target_fd = open("/dev/cpu_dma_latency", O_RDWR);
-        if (latency_target_fd == -1)
-            return;
-        // BUG: write returns size_t ret is int -> implicit narrowing!
-        ret = write(latency_target_fd, &latency_target_value, 4);
-        if (ret == 0) {
-            // SMELL: not C++
-            printf("# error setting cpu_dma_latency to %d!: %s\n", latency_target_value, strerror(errno));
-            // BUG: file gets only closed when write successful
-            close(latency_target_fd);
-            return;
-        }
-        // SMELL: not error Handling or
-        // IMPROVE: notification if unsuccessful (write, open)
-        // SMELL: not c++
-        printf("# /dev/cpu_dma_latency set to %dus\n", latency_target_value);
-    }
+void
+set_latency_target(
+	void
+) {
+	struct stat file_status{};
+
+	int error;
+
+	latency_target_path = "/dev/cpu_dma_latency";
+
+	error = stat(latency_target_path, &file_status);
+
+	if (error == 0) {
+		int latency_target_fd = open(latency_target_path, O_RDWR);
+		if (latency_target_fd == -1) {
+			return;
+		}
+		
+		int ret;
+		int32_t latency_target_value = 0;
+
+		size_t bytes_written = write(latency_target_fd, &latency_target_value, sizeof(latency_target_value));
+		if (bytes_written == -1) {
+			err(EXIT_FAILURE, "could not set cpu_dma_latency to %d: %s\n", latency_target_value, latency_target_path);
+			close(latency_target_fd);
+
+			return;
+		}
+	}
+
+	return;
 }
 
 
-void signalHandler(int signum) {
-    running = 0;
-    // SMELL: absolute useless statement.
-    signum = signum;
-    // BUG: not signal safe. (join, Debug::error,close,exit ,...) not signal safe. This function is a nightmare for safety!!!
-    // IMPROVE: exit outside of main. (exit should only be used in main)
+int
+main(
+	int argc,
+	char const *argv[]
+) {
+	struct options options{};
+
+	get_options(argc, argv, &options);
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 0) {
+		usage();
+	}
+
+	set_scheduling_priority(60);
+
+	set_latency_target();
+
+	// IMPROVE: this should be refactored to own function
+	signal(SIGINT, signalHandler);
+	signal(SIGTERM, signalHandler);
+	signal(SIGABRT, signalHandler);
+
+	// SMELL: don't test the program with itself use a test program
 #ifdef TEST_LLSERVER
-    if (testThread != nullptr && testThread->joinable())
-    {
-        testThread->join();
-        delete testThread;
-    }
-
+	testThread = new std::thread(testFnc);
 #endif
-
-    Debug::error("posix signal fired: %d, shutting down...", signum);
-
-    try {
-        LLController::Destroy();
-    }
-    catch (std::exception &e) {
-        Debug::error("signal handler: failed to shutdown LLController, %s", e.what());
-    }
-
-    Debug::close();
-
-    exit(signum);
-}
-
-int main(int argc, char const *argv[]) {
-    // SMELL: System opens a new shell. This is not necessary. Remove line!
-    system("clear");
-    // IMPROVE: uninitialized struct type
-    struct sched_param sp;
-    sp.sched_priority = 60;
-    // IMPROVE: This makes the Code only compilable on linux. Make it optional for test version. (enables Multiplatform development)
-    sched_setscheduler(0, SCHED_FIFO, &sp);
-
-    set_latency_target();
-
-    // IMPROVE: this should be refactored to own function
-    signal(SIGINT, signalHandler);
-    signal(SIGTERM, signalHandler);
-    signal(SIGABRT, signalHandler);
-
-    // SMELL: inconsistent Naming and redundant improve by removing initialization
-    std::string configPath = "";
-    // IMPROVE: wtf? why config-file for config-file-path?
-    if (argc > 1) {
-        // SMELL: cfgPath variable is not needed in this case
-        std::string cfgPath(argv[1]);
-        configPath = cfgPath;
-        // IMPROVE: redundant information this is defined behavior
-        std::cout << "Config path given as argument, using this." << std::endl;
-    } else {
-        std::ifstream configPathFile(CONFIG_PATH_FILE);
-        if (configPathFile.fail()) {
-            // IMPROVE: use errno messages
-            std::cerr << "Config path file " << CONFIG_PATH_FILE << " is missing or could not be read!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        // SMELL: no error handling
-        std::getline(configPathFile, configPath);
-        // IMPROVEMENT: redundant information this is defined behavior
-        std::cout << "Using config path in " << CONFIG_PATH_FILE << std::endl;
-    }
-
-    std::cout << "Config path: " << configPath << std::endl;
-    // SMELL: don't test the program with itself use a test program
-#ifdef TEST_LLSERVER
-    testThread = new std::thread(testFnc);
-#endif
-    // IMPROVE: use normal class with shared pointers: This is cleaner and more efficient.
-    LLController *llController = LLController::Instance();
-    llController->Init(configPath);
-    // SMELL: not used string
-    std::string inputStr;
-    // SMELL: NOT BUSY WAITING !!! Why not abandon thread?
-    while (running) {
-        sleep(1);
-    }
+	// IMPROVE: use normal class with shared pointers: This is cleaner and more efficient.
+	LLController *llController = LLController::Instance();
+	llController->Init(options.config_path);
+	// SMELL: not used string
+	std::string inputStr;
+	// SMELL: NOT BUSY WAITING !!! Why not abandon thread?
+	while (running) {
+		sleep(1);
+	}
 }
 
