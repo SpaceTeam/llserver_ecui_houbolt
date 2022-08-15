44fbb40a7c94a8869ac8bb013c4eefcf2b0b6840
Lukas Karafiat <lukas.karafiat@spaceteam.at>
2022-08-15 20:23:58 +0200

    add: initial skeleton of multi consumer, multi producer queue
    Reviewer: Christofer Held

--- include/server/Queue.h
+++ include/server/Queue.h
@@ -1,10 +1,22 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
+#include <queue>
+
+// NOTE(Lukas Karafiat): this is a bottleneck, but an ordered sequential bottleneck.  exactly what we want!
+// multi producer, multi consumer queue with a central repository of relevant data
 class Queue {
 private:
+	std::queue<int> buffer;
 
-public: 
+	// TODO: lock(1,2) | blocks if another thread wants to write/read to the queue
+	// TODO: semaphore | knows how many things are in the queue to read and blocks when there are none
+public:
+	Queue(void);
+	~Queue(void);
+
+	void push(int);
+	int pop(void);
 };
 
 #endif /* QUEUE_HPP */
--- src/server/Queue.cpp
+++ src/server/Queue.cpp
@@ -1,1 +1,41 @@
 #include "server/Queue.h"
+
+Queue::Queue(
+	void
+) {
+	return;
+}
+
+
+Queue::~Queue(
+	void
+) {
+	return;
+}
+
+
+void
+Queue::push(
+	int value
+) {
+	// 1. acquire lock(1)
+	// 2. push
+	// 3. increase semaphore
+	// 4. release lock(1)
+
+	return;
+}
+
+
+int
+pop(
+	void
+) {
+	// 1. acquire lock(2)
+	// 2. pop
+	// 3. decrease semaphore
+	// 4. relesase lock(2)
+
+	return 0;
+}
+
