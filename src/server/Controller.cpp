f159a454a2498ddea8696a4029eafad988242949
Lukas Karafiat <lukas.karafiat@spaceteam.at>
2022-08-17 21:32:52 +0200

    add: read and write loop skeletons  of web socket controller
    Reviewer: Christofer Held

--- src/server/Controller.cpp
+++ src/server/Controller.cpp
@@ -1,11 +1,16 @@
 #include "server/Controller.h"
 
 #include <iostream>
+#include <thread>
 
 Controller::Controller(
-	Queue& request_queue,
-	Queue& response_queue
-) {
+	std::shared_ptr<Queue>& request_queue,
+	std::shared_ptr<Queue>& response_queue
+) :
+	socket("127.0.0.1", "8080"),
+	request_queue(request_queue),
+	response_queue(response_queue)
+{
 	return;
 }
 
@@ -14,7 +19,39 @@ void
 Controller::run(
 	void
 ) {
-	std::cout << "controller" << std::endl;
+	std::jthread read_thread(&Controller::read_loop, *this);
+	write_loop();
+
+	return;
+}
+
+
+void
+Controller::read_loop(
+	void
+) {
+	extern bool finished;
+
+	while(!finished) {
+		socket.receive();
+		request_queue->push(1);
+	}
+
+	return;
+}
+
+
+void
+Controller::write_loop(
+	void
+) {
+	extern bool finished;
+
+	while(!finished) {
+		int value = request_queue->pop();
+		std::string payload = "sample";
+		socket.send(payload);
+	}
 
 	return;
 }
