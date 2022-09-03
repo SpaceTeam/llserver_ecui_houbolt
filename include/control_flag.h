37c4e40115a8a1b70fef4493e7c933e88e6af7eb
christofer held <christofer.held@spaceteam.at>
2022-09-03 15:49:42 +0200

    add: added blocking mode to ringbuffer and builded construct for logging

--- include/control_flag.h
+++ include/control_flag.h
@@ -3,6 +3,11 @@
 
 #include <signal.h>
 
+#define LOGGING_FILE_PATH "logs/log.txt"
+
 extern volatile sig_atomic_t finished;
+
+const extern volatile bool logging_in_influx = true;
+const extern volatile bool logging_in_file = true;
 
 #endif /* CONTROL_FLAG */
