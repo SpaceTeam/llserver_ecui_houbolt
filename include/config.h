40cc7faf77db0ab01bef4a5e3e61ef04fe2fa392
Lukas Karafiat <lukas.karafiat@spaceteam.at>
2022-09-04 12:18:38 +0200

    change: compile time config in file and not as macros

---
+++ include/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const int minimum_severity = 0;
+
+const bool log_to_console = true;
+const bool log_to_file = true;
+const bool log_to_influx_db = false;
+
+#endif /* CONFIG_H */
--- include/utility/Logger.h
+++ include/utility/Logger.h
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <iomanip>
 
+#include "config.h"
+
 enum Severity {
 	TRACE = 0,
 	DEBUG = 1,
@@ -25,37 +27,35 @@ log(
 	std::string source,
 	std::string message
 ) {
-	auto system_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-	auto iso_time = std::put_time(std::localtime(&system_time), "%F %T");
-
-	std::stringstream log_message;
-
-	log_message << severity_names[severity] << ":" << source << ":" << iso_time << "\t\t" << message << std::endl;
-
-	if (severity < MINIMUM_SEVERITY) {
+	if constexpr (severity < minimum_severity) {
 		return;
-	}
-
-#ifdef LOG_CONSOLE
-	if (severity < Severity::WARNING) {
-		std::cout << log_message.str();
 
 	} else {
-		std::cerr << log_message.str();
-	}
-#endif
+		auto system_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+		auto iso_time = std::put_time(std::localtime(&system_time), "%F %T");
 
-#ifdef LOG_FILE
-	{
-		std::ofstream log_file(LOG_FILE_PATH, std::ios_base::app | std::ios_base::out);
-		log_file << log_message.str();
-		log_file.close();
-	}
-#endif
+		std::stringstream log_message;
+		log_message << severity_names[severity] << ":" << source << ":" << iso_time << "\t\t" << message << std::endl;
 
-#ifdef LOG_INFLUX
-	// TODO(Christofer Held): implement this
-#endif
+		if constexpr (log_to_console) {
+			if constexpr (severity < Severity::WARNING) {
+				std::cout << log_message.str();
+
+			} else {
+				std::cerr << log_message.str();
+			}
+		}
+
+		if constexpr (log_to_file) {
+			std::ofstream log_file(LOG_FILE_PATH, std::ios_base::app | std::ios_base::out);
+			log_file << log_message.str();
+			log_file.close();
+		}
+
+		if constexpr (log_to_influx_db) {
+			// TODO(Christofer Held): implement this
+		}
+	}
 
 	return;
 }
