9e6df7b3066573b7600ab0125a5dda5258ee46a0
Lukas Karafiat <lukas.karafiat@spaceteam.at>
2022-09-08 21:51:34 +0200

    add: peripherie class for reading and writing sensor/actuator data

---
+++ include/Peripherie.h
@@ -0,0 +1,35 @@
+#ifndef PERIPHERIE_H
+#define PERIPHERIE_H
+
+#include "utility/RingBuffer.h"
+
+#include "peripherie/Frame.h"
+
+#include <memory>
+#include <string>
+
+class Peripherie {
+private:
+	std::shared_ptr<RingBuffer<struct peripherie_frame>> input_queue;
+	std::shared_ptr<RingBuffer<struct peripherie_frame>> output_queue;
+
+public:
+	Peripherie() = delete;
+	// NOTE(Lukas Karafiat): the function declaration got out of hand, had to shorten it quite a bit
+	explicit Peripherie(
+		std::shared_ptr<RingBuffer<struct peripherie_frame>>&input_queue,
+		std::shared_ptr<RingBuffer<struct peripherie_frame>>&output_queue);
+	~Peripherie();
+
+	// non copyable
+	Peripherie(Peripherie const &) = delete;
+	void operator=(Peripherie const &x) = delete;
+
+	// movable
+	Peripherie(Peripherie &&) = default;
+	Peripherie& operator=(Peripherie &&x) = default;
+
+	void run(void);
+};
+
+#endif /* PERIPHERIE_H */
---
+++ src/Peripherie.cpp
@@ -0,0 +1,23 @@
+#include "Peripherie.h"
+
+Peripherie::Peripherie(
+	std::shared_ptr<RingBuffer<struct peripherie_frame>>&input_queue,
+	std::shared_ptr<RingBuffer<struct peripherie_frame>>&output_queue
+) {
+	return;
+}
+
+
+Peripherie::~Peripherie(
+	void
+) {
+	return;
+}
+
+void
+Peripherie::run(
+	void
+) {
+	return;
+}
+
