59f3940b3d96be88bd2759ae1f8d21ae7c862ef5
markuspinter <markus.m.pinter@gmail.com>
2023-01-17 13:36:28 +0100

    update update script

--- update.sh
+++ update.sh
@@ -4,8 +4,4 @@ cd "$(dirname "$0")"
 
 git pull
 
-cmake . -DCMAKE_BUILD_TYPE:STRING=Debug -DNO_CANLIB:BOOL=True -DNO_PYTHON:BOOL=True
-
-make -j3
-
-sudo systemctl restart ecui-llserver.service
+exec ./install.sh
