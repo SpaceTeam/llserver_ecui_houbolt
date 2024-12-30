296bcefcce4a5061593ba20853a9d40120638171
Raffael Rott <raffaelrott@protonmail.com>
2024-12-30 01:09:10 +0100

    - Fixed a Bug which crashed the llserver when the logs directory didn't exist (It now creates it)

--- src/SequenceManager.cpp
+++ src/SequenceManager.cpp
@@ -138,6 +138,9 @@ void SequenceManager::SetupLogging()
     strftime(dateTime_string, 100, "%Y_%m_%d__%H_%M_%S", curr_tm);
 
     currentDirPath = "logs/" + std::string(dateTime_string);
+    std::experimental::filesystem::create_directory("logs");
+    std::experimental::filesystem::create_directory(currentDirPath);
+
     this->lastDir = currentDirPath;
     logFileName = std::string(dateTime_string) + ".csv";
     std::experimental::filesystem::create_directory(currentDirPath);
@@ -403,7 +406,7 @@ void SequenceManager::sequenceLoop(int64_t interval_us)
 		if(sequenceTime_us >= nextTimePrint_us)
 		{
 			Debug::info("Sequence Time: %dus", sequenceTime_us);
-			nextTimePrint_us += 500000;
+			nextTimePrint_us += 300000;
 		}
 
 		static int32_t nextTimerSync_us = startTime_us;
