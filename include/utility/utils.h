bb38b160c7638a99d9c4a69bbc8f2b2cb6985468
Raffael Rott <raffaelrott@protonmail.com>
2025-05-05 19:47:07 +0200

    - Simple test now works.

--- include/utility/utils.h
+++ include/utility/utils.h
@@ -6,7 +6,6 @@
 #define TXV_ECUI_LLSERVER_UTILS_H
 
 #include <fstream>
-#include <sstream>
 
 #include "common.h"
 
@@ -17,8 +16,6 @@ namespace utils
 
     int64_t toMicros(double val);
 
-    std::string loadFile(std::string filePath);
-    void saveFile(std::string filePath, std::string content);
 
     bool keyExists(const nlohmann::json& j, const std::string& key);
 
