4de55433d7fa6302c8cf9f063eb5d358e8174976
Raffael Rott <raffaelrott@protonmail.com>
2025-05-25 22:54:44 +0200

    - Added a test which checks if items are executed in time-order if they are swapped in the json. Currently the test fails

--- tests/data/SequenceManagerTestData.h
+++ tests/data/SequenceManagerTestData.h
@@ -90,4 +90,69 @@ inline nlohmann::json SimpleAbortScenario_json = R"(
   }
 }
 )"_json;
+
+inline nlohmann::json TimeStampsSwapped_json = R"({
+  "globals": {
+    "endTime": 1,
+    "interpolation": {
+      "valve_1": "none"
+    },
+    "interval": 0.01,
+    "startTime": 0
+  },
+  "data": [
+    {
+      "timestamp": "START",
+      "name": "start",
+      "desc": "start",
+      "actions": [
+        {
+          "timestamp": 0.0,
+          "valve_1": [
+            0
+          ]
+        }
+      ]
+    },
+    {
+      "timestamp": 0.4,
+      "name": "4.0",
+      "desc": "4.0",
+      "actions": [
+        {
+          "timestamp": 0.0,
+          "valve_1": [
+            50
+          ]
+        }
+      ]
+    },
+    {
+      "timestamp": 0.2,
+      "name": "2.0",
+      "desc": "2.0",
+      "actions": [
+        {
+          "timestamp": 0.0,
+          "valve_1": [
+            20
+          ]
+        }
+      ]
+    },
+    {
+      "timestamp": "END",
+      "name": "end",
+      "desc": "end",
+      "actions": [
+        {
+          "timestamp": 0.0,
+          "valve_1": [
+            100
+          ]
+        }
+      ]
+    }
+  ]
+})"_json;
 #endif //SEQUENCEMANAGERTESTDATA_H
